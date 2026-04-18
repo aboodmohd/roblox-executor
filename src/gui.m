59a46701f62eeff23cdf7e6f039481a6369aa3e7
abd009es <123668866+aboodmohd@users.noreply.github.com>
2026-04-18 12:34:07 +0400

    Add text input field and execute button to GUI

--- src/gui.m
+++ src/gui.m
@@ -2,16 +2,37 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @property (strong) NSWindow *window;
+@property (strong) NSTextField *scriptInput;
 @end
 
 @implementation AppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    self.window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 200)
+    self.window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 150)
                                               styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable)
                                                 backing:NSBackingStoreBuffered
                                                   defer:NO];
     [self.window setTitle:@"Roblox Executor"];
+
+    // Script Input
+    self.scriptInput = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 60, 360, 30)];
+    [self.scriptInput setPlaceholderString:@"Paste script URL here..."];
+    [[self.window contentView] addSubview:self.scriptInput];
+
+    // Execute Button
+    NSButton *executeBtn = [[NSButton alloc] initWithFrame:NSMakeRect(150, 20, 100, 30)];
+    [executeBtn setTitle:@"Execute"];
+    [executeBtn setBezelStyle:NSBezelStyleRounded];
+    [executeBtn setTarget:self];
+    [executeBtn setAction:@selector(executeScript:)];
+    [[self.window contentView] addSubview:executeBtn];
+
     [self.window makeKeyAndOrderFront:nil];
+}
+
+- (void)executeScript:(id)sender {
+    NSString *input = [self.scriptInput stringValue];
+    NSLog(@"Executing script from: %@", input);
+    // Logic to call the C++ execution backend would go here
 }
 @end
 
